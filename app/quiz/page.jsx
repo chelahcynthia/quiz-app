2eab64d88fbcaf59d5d4e9b8cb100ad895b8f922
chelahcynthia <cchelangat989@gmail.com>
2023-06-21 14:54:06 +0300

    Error fixes

--- app/quiz/page.jsx
+++ app/quiz/page.jsx
@@ -18,39 +18,42 @@ function page() {
   const { question, answers, correctAnswer} = questions[activeQuestion];
 
   //  Select and check answer
-  const onAnswerSelected = {answer, idx} => {
+  const onAnswerSelected = (answer, idx) => {
     setChecked(true);
     setSelectedAnswerIndex(idx);
     if (answer === correctAnswer) {
       setSelectedAnswer(true);
+      console.log('true');
+    } else {
+      setSelectedAnswer(false);
       console.log('false');
     }
+  
   };
 
   // calculate score and increment to next question
-  const nextQuestion = () {
+  const nextQuestion = () => {
     setSelectedAnswerIndex(null);
     setResult((prev) =>
-    selectedAnswer 
-    ? {
-      ...prev,
-      score: prev.score + 5,
-      correctAnswers: prev.correctAnswers + 1,
-
-    }
-    : {
-      ...prev,
-      wrongAnswers: prev.wrongAnswers + 1,
-    }
+      selectedAnswer
+        ? {
+            ...prev,
+            score: prev.score + 5,
+            correctAnswers: prev.correctAnswers + 1,
+          }
+        : {
+            ...prev,
+            wrongAnswers: prev.wrongAnswers + 1,
+          }
     );
-    if ( activeQuestion !== questions.length -1) {
-      setActveQuestion((prev) => prev +1);
-
-    }else{
-      setActveQuestion(0);
+    if (activeQuestion !== questions.length - 1) {
+      setActiveQuestion((prev) => prev + 1);
+    } else {
+      setActiveQuestion(0);
       setShowResult(true);
     }
     setChecked(false);
+   
 
   };
 
